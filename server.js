a0bd477cbd1d45229021d7038b3112db44a50118
Poly Goveia <poly.goveia@gmail.com>
2019-08-15 20:12:15 -0700

    add database and migrations

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 const express = require("express");
 
-const carsRouter = require("./cars/carsRouter");
+const helmet = require("helmet");
+
+const carsRouter = require("./data/cars-router.js");
 
 const server = express();
 
-server.get("/", (req, res) => {
-  res.send(`<h2>WebDB II Challenge</h2>`);
-});
+server.use(helmet());
 
 server.use(express.json());
 
 server.use("/api/cars", carsRouter);
-
-server.use(errorHandler);
-
-function errorHandler(error, req, res, next) {
-  console.log(error);
-  res.status(500).json({ error: "Data could not be retrieved" });
-}
 
 module.exports = server;
